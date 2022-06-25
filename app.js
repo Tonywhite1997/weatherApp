52135d5f57ed5a024ac13425e0a4e6288a8c1627
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2022-06-25 14:25:24 +0100

    added more functionalities

--- app.js
+++ app.js
@@ -1,40 +1,85 @@
-window.addEventListener("load", ()=>{
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(getPosition)
-    }
-    else{
-        return alert("Geolocation not supported in browser.")
-    }
-})
-
 const country = document.querySelector(".main__region")
 const temperature = document.querySelector(".main__temperature")
 const description = document.querySelector(".main__weather__description")
 const degree = document.querySelector(".degrees")
 const changeDegree = document.querySelector(".temp")
+const currentDate = document.querySelector(".date")
+let otherLatitude = document.querySelector(".latitudeInput")
+let otherLongitude = document.querySelector(".longitudeInput")
+let searchButton =  document.querySelector(".dateButton")
+let currentLocationBtn = document.querySelector(".currentDateButton")
+const errorText = document.querySelector(".error__text")
+
+let coordinates ={
+    latitude: null,
+    longitude: null,
+}
 
 
-async function getPosition(position){
+async function getYourPosition(position){
     let latitude = position.coords.latitude
     let longitude = position.coords.longitude
 
-    let res = await axios.get("https://api.openweathermap.org/data/2.5/weather?", {
+    let coordinates ={
+        latitude,
+        longitude,
+    }
+    localStorage.setItem("coordinates", JSON.stringify(coordinates))
+    let myCoordinates = JSON.parse(localStorage.getItem("coordinates"))
+
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/onecall?", {
         params:{
-            lat: latitude,
-            lon: longitude,
+            lat: myCoordinates.latitude,
+            lon: myCoordinates.longitude,
+            appid: "9a4f4bb3e9b9706556c90de2a41fb2db"
+        }
+    })
+    
+    let weatherData = res.data
+
+    let weatherDate = new Date(weatherData.current.dt * 1000).toLocaleString();
+    country.innerText = weatherData.timezone
+    let tempIndegree = Math.floor(weatherData.daily[0].temp.day + (-273.15))
+    temperature.innerText = tempIndegree
+    degree.innerText = "C"
+    description.innerText = weatherData.daily[0].weather[0].description
+    currentDate.innerText = weatherDate;
+
+    toggleDegree(weatherData)
+}
+
+async function getOtherPosition(){
+    let coordinates ={
+        latitude: otherLatitude.value,
+        longitude: otherLongitude.value,
+    }
+    localStorage.setItem("coordinates", JSON.stringify(coordinates))
+    let newCoordinates = JSON.parse(localStorage.getItem("coordinates"))
+
+    try{
+        let res = await axios.get("http://api.openweathermap.org/data/2.5/onecall?", {
+        params:{
+            lat: newCoordinates.latitude,
+            lon: newCoordinates.longitude,
             appid: "9a4f4bb3e9b9706556c90de2a41fb2db"
         }
     })
 
     let weatherData = res.data
-    // console.log(weatherData);
-    country.innerText = weatherData.sys.country
-    let tempIndegree = Math.floor(weatherData.main.temp + (-273.15))
+
+    let weatherDate = new Date(weatherData.current.dt * 1000).toLocaleString();
+    country.innerText = weatherData.timezone
+    let tempIndegree = Math.floor(weatherData.daily[0].temp.day + (-273.15))
     temperature.innerText = tempIndegree
     degree.innerText = "C"
-    description.innerText = weatherData.weather[0].description
+    description.innerText = weatherData.daily[0].weather[0].description
+    currentDate.innerText = weatherDate;
 
     toggleDegree(weatherData)
+    }
+    catch(err){
+        errorText.innerText = `${err.response.status}:  ${err.response.data.message.toUpperCase()}. Try again`;
+    }
 }
 
 function toggleDegree(data){
@@ -49,9 +94,25 @@ function toggleDegree(data){
             temperature.innerText = tempIndegree
             degree.innerText = "C"
         }
-        
-
-        
-})
+    })
 }
 
+searchButton.addEventListener("click", ()=>{
+    errorText.innerText = ""
+    country.innerText = ""
+    temperature.innerText = ""
+    description.innerText = ""
+    currentDate.innerText = "";
+    degree.innerText = ""
+    getOtherPosition()
+})
+
+currentLocationBtn.addEventListener("click", ()=>{
+    errorText.innerText = ""
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(getYourPosition)
+    }
+    else{
+        console.log("Navigator not supported in browser.");
+    }
+})
